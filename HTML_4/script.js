e7b9517559843d4f13ff384811779dd274b4298c
cristian8data <cristian.8.data@gmail.com>
2025-06-04 09:07:30 +0200

    Subida archivos

---
+++ HTML_4/script.js
@@ -0,0 +1,7 @@
+// Mostrar mensaje al cargar la página
+alert("¡Bienvenido a la página del Real Madrid!");
+
+// Al hacer clic en el título
+document.querySelector("h1").addEventListener("click", function () {
+  alert("¡Hala Madrid!");
+});
