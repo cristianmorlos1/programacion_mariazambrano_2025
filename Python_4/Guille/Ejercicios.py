5c15d6a64606348e65f263fdfe9b6501a1064447
GUILLERMO DE GREGORIO CRIADO <guillermo.grecri@educa.jcyl.es>
2025-06-09 14:14:18 +0200

    Añadido los ejercicios

---
+++ Python_4/Guille/Ejercicios.py
@@ -0,0 +1,14 @@
+
+#Ejercicios de nivel 2
+
+print("Ejercicios de nivel 2:")
+
+    #Ejercicio 4
+
+edad=int(input("Escribe tu edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
