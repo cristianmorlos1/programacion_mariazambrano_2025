448ae795d05b7cec36a004e83e77865e8c6deee9
ADRIÁN RODRÍGUEZ DE PABLOS <adrian.rodpab@educa.jcyl.es>
2025-06-11 12:24:46 +0200

    he subido cambios

---
+++ HTML_4/script-adri.js
@@ -0,0 +1,7 @@
+// Mostrar mensaje al cargar la página
+alert("¡Bienvenido a la wiki de Hades!");
+
+// Al hacer clic en el título
+document.querySelector("h1").addEventListener("click", function () {
+  alert("¡Qué juegazo por dios!");
+});
